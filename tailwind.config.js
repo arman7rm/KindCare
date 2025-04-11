0f5e30e210a99fac19fe2b1f3e28bb2026d37ea3
arman7rm <arman97rm@gmail.com>
2025-04-11 01:35:32 -0400

    styling changes

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+    corePlugins: {
+      transform: true,       // Must be true
+      transitionProperty: true,
+      transitionDuration: true,
+      transitionTimingFunction: true
+    }
+  }
